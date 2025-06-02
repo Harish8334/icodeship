b7a1fff132ee7fa3f97a854c37528ff6aeb74255
kishore-404 <kishores.developer@gmail.com>
2025-06-02 18:15:25 +0530

    test 3

--- src/Data/Office_data.jsx
+++ src/Data/Office_data.jsx
@@ -1,13 +1,13 @@
-import Office_img1 from "../assets/images/About/office/about_office1.jpg";
-import Office_img2 from "../assets/images/About/office/about_office2.jpg";
-import Office_img3 from "../assets/images/About/office/about_office3.jpg";
-import Office_img4 from "../assets/images/About/office/about_office4.jpg";
+import Office_img1 from "../assets/images/About/office/about_office1.webp";
+import Office_img2 from "../assets/images/About/office/about_office2.webp";
+import Office_img3 from "../assets/images/About/office/about_office3.webp";
+import Office_img4 from "../assets/images/About/office/about_office4.webp";
 
 const Office_Data = [
   { img: Office_img1 },
   { img: Office_img2 },
   { img: Office_img3 },
-  { img: Office_img4 }
+  { img: Office_img4 },
 ];
 
 export default Office_Data;
