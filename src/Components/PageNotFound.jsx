67cebd9e371326b2ae7b0aaa6cc237eb75cab0c0
Harish8334 <harish8334@gmail.com>
2025-07-03 15:29:23 +0530

    not found page

--- src/Components/PageNotFound.jsx
+++ src/Components/PageNotFound.jsx
@@ -1,6 +1,6 @@
 import pagenotfound from "../assets/images/pageNotFound.jpg";
 
-export const PageNotFound = () => {
+export default function PageNotFound () {
   return (
     <div
       className="d-flex justify-content-center align-items-center vh-100  bg-white mt-5"
