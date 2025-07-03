0957bcc3522686de9790e092b5f32e9eab39938d
Harish8334 <harish8334@gmail.com>
2025-07-03 18:00:45 +0530

    content update

--- src/Data/Contact_Accordion.jsx
+++ src/Data/Contact_Accordion.jsx
@@ -2,30 +2,30 @@
 const leftAccordionData = [
   {
     title: "Why should I consider hiring a software development agency?",
-    content: "iCodeShip’s expert team creates innovative, custom software solutions to help your business grow. We simplify development, lower risks, and provide quick, scalable results, allowing you to focus on growth."
+    content: "CodeShip’s expert team creates innovative, custom software solutions to help your business grow. We simplify development, lower risks, and provide quick, scalable results, allowing you to focus on growth."
   },
   {
     title: "How can partnering with a software development agency save us budget?",
-    content: "Working with iCodeShip makes the best use of your budget by using our skilled team to deliver efficient, high-quality software solutions. We lower risks and cut long-term costs through simplified development and scalable results."
+    content: "Working with CodeShip makes the best use of your budget by using our skilled team to deliver efficient, high-quality software solutions. We lower risks and cut long-term costs through simplified development and scalable results."
   },
   {
     title: "In which industries does Codeship have experience?",
-    content: "iCodeShip works well in various industries, providing custom software solutions with great expertise. Our strong skills in web and mobile development, UI/UX design, and dependable support services help your business succeed."
+    content: "CodeShip works well in various industries, providing custom software solutions with great expertise. Our strong skills in web and mobile development, UI/UX design, and dependable support services help your business succeed."
   }
   ];
   
  const rightAccordionData = [
   {
-    title: "What sets Outecho apart from other software development agencies?",
-    content: "iCodeShip stands out because we focus on delivering custom software that helps your business grow. Our client-first approach allows for quick, high-quality results and smooth collaboration at every step."
+    title: "What sets codeship apart from other software development agencies?",
+    content: "CodeShip stands out because we focus on delivering custom software that helps your business grow. Our client-first approach allows for quick, high-quality results and smooth collaboration at every step."
   },
   {
     title: "What technologies does Codeship use for development?",
-    content: "iCodeShip uses a robust tech stack that includes microservices architecture, containerization with Docker and Kubernetes, modern frameworks, and cloud solutions on AWS and Azure to provide scalable software that’s ready for the future."
+    content: "CodeShip uses a robust tech stack that includes microservices architecture, containerization with Docker and Kubernetes, modern frameworks, and cloud solutions on AWS and Azure to provide scalable software that’s ready for the future."
   },
   {
     title: "Can you provide support for existing websites or apps?",
-    content: "Absolutely, iCodeShip offers expert support for existing websites and apps. We ensure optimal performance and smooth updates. Our dedicated team provides custom maintenance and improvements to keep your digital solutions successful."
+    content: "Absolutely, CodeShip offers expert support for existing websites and apps. We ensure optimal performance and smooth updates. Our dedicated team provides custom maintenance and improvements to keep your digital solutions successful."
   }
   ];
   export { leftAccordionData, rightAccordionData };
